6f8f38ec8cccdc609b3d21983dae3fa3177114f2
Vedat Zeybek <vzeybek@k1m43s03.42istanbul.com.tr>
2025-07-02 10:47:55 +0300

    third change

--- pipex.c
+++ pipex.c
@@ -5,8 +5,6 @@
 #include "stdlib.h"
 #include "sys/wait.h"
 
-
-
 char	*get_env(char **env)
 {
 	int 	i;
@@ -48,57 +46,60 @@ char	*get_path(char *cmd, char **env)
 	return (NULL);
 }
 
-
-char	*parse_args()
-{
-	
-}
-
-
-int main(int argc, char **argv, char **env)
+int	child_process1(int fd_in, int fd_out, int *pipefd ,char **argv, char **env)
 {
 	pid_t pid0;
-	pid_t pid1;
-	int	status;
-	int fd_in = open("input.txt", O_CREAT | O_RDWR, 0644);
-	int fd_out = open("output.txt", O_CREAT | O_RDWR, 0644);
-	int pipefd[2];
+	char *str[20];
 
-	pipe(pipefd);
+	ft_split(argv[2], ' ');
 	pid0 = fork();
 	if (pid0 == 0)
 	{
 		dup2(fd_in, STDIN_FILENO);
 		dup2(pipefd[1], STDOUT_FILENO);
-		char *str2[] = {"grep", "pipex.c", NULL};
 		close(pipefd[0]);
 		close(pipefd[1]);
 		close(fd_in);
 		close(fd_out);
-		execve("/usr/bin/grep", str2, env);
+		execve(get_path(str[0], env), str, env);
 	}
+	return (pid0);
+}
 
+int	child_process2(int fd_in, int fd_out, int *pipefd ,char **argv, char **env)
+{
+	pid_t pid1;
+	char **str;
+
+	str = ft_split(argv[3], ' ');
 	pid1 = fork();
 	if (pid1 == 0)
 	{
 		dup2(pipefd[0], STDIN_FILENO);
 		dup2(fd_out, STDOUT_FILENO);
-		char *str1[] = {"cat", NULL};
 		close(pipefd[1]);
 		close(pipefd[0]);
 		close(fd_in);
 		close(fd_out);
-		execve("/usr/bin/cat", str1, env);
-
+		execve(get_path(str[0], env), str, env);
 	}
-	int i;
+	return (pid1);
+}
 
-	i = 0;
-	while (env[i])
-	{
-		printf("%s\n", env[i]);
-		i++;
-	}
+int main(int argc, char **argv, char **env)
+{
+	int	status;
+	int fd_in = open(argv[1], O_CREAT | O_RDWR, 0644);
+	int fd_out = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	int pipefd[2];
+	pid_t	pid0;
+	pid_t	pid1;
+	if (argc != 5)
+		return (1);
+
+	pipe(pipefd);
+	child_process1(fd_in, fd_out, pipefd ,argv, env);
+	child_process2(fd_in, fd_out, pipefd ,argv, env);
 	close(pipefd[1]);
 	close(pipefd[0]);
 	close(fd_in);
