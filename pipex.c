7fc3326ad73ecbfae07e8bfd7b60e2f5e8a84e3e
Vedat Zeybek <vzeybek@k1m43s03.42istanbul.com.tr>
2025-07-02 11:17:33 +0300

    m

--- pipex.c
+++ pipex.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vzeybek <vzeybek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/02 11:08:06 by vzeybek           #+#    #+#             */
+/*   Updated: 2025/07/02 11:16:12 by vzeybek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./libft/libft.h"
 #include "unistd.h"
 #include "fcntl.h"
@@ -7,17 +19,17 @@
 
 char	*get_env(char **env)
 {
-	int 	i;
-	char *str;
+	int		i;
+	char	*str;
 
 	i = 0;
-	while(env[i])
+	while (env[i])
 	{
 		if (ft_strncmp(env[i], "PATH=", 5) == 0)
-			{
-				str = ft_substr(env[i], 5, ft_strlen(env[i]));
-				return (str);
-			}
+		{
+			str = ft_substr(env[i], 5, ft_strlen(env[i]));
+			return (str);
+		}
 		i++;
 	}
 	return (NULL);
@@ -45,64 +57,64 @@ char	*get_path(char *cmd, char **env)
 	return (NULL);
 }
 
-int	child_process1(int fd_in, int fd_out, int *pipefd ,char **argv, char **env)
+int	child_process1(int *fd, int *pipefd, char **argv, char **env)
 {
-	pid_t pid0;
-	char **str;
+	pid_t	pid0;
+	char	**str;
 
 	str = ft_split(argv[2], ' ');
 	pid0 = fork();
 	if (pid0 == 0)
 	{
-		dup2(fd_in, STDIN_FILENO);
+		dup2(fd[0], STDIN_FILENO);
 		dup2(pipefd[1], STDOUT_FILENO);
 		close(pipefd[0]);
 		close(pipefd[1]);
-		close(fd_in);
-		close(fd_out);
+		close(fd[0]);
+		close(fd[1]);
 		execve(get_path(str[0], env), str, env);
 	}
 	return (pid0);
 }
 
-int	child_process2(int fd_in, int fd_out, int *pipefd ,char **argv, char **env)
+int	child_process2(int *fd, int *pipefd, char **argv, char **env)
 {
-	pid_t pid1;
-	char **str;
+	pid_t	pid1;
+	char	**str;
 
 	str = ft_split(argv[3], ' ');
 	pid1 = fork();
 	if (pid1 == 0)
 	{
 		dup2(pipefd[0], STDIN_FILENO);
-		dup2(fd_out, STDOUT_FILENO);
+		dup2(fd[1], STDOUT_FILENO);
 		close(pipefd[1]);
 		close(pipefd[0]);
-		close(fd_in);
-		close(fd_out);
+		close(fd[0]);
+		close(fd[1]);
 		execve(get_path(str[0], env), str, env);
 	}
 	return (pid1);
 }
 
-int main(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **env)
 {
-	int	status;
-	int fd_in = open(argv[1], O_CREAT | O_RDWR, 0644);
-	int fd_out = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	int pipefd[2];
+	int		fd[2];
+	int		pipefd[2];
 	pid_t	pid0;
 	pid_t	pid1;
+
 	if (argc != 5)
 		return (1);
-
+	fd[0] = open(argv[1], O_CREAT | O_RDWR, 0644);
+	fd[1] = open(argv[4], O_CREAT | O_RDWR, 0644);
 	pipe(pipefd);
-	child_process1(fd_in, fd_out, pipefd ,argv, env);
-	child_process2(fd_in, fd_out, pipefd ,argv, env);
+	child_process1(fd, pipefd, argv, env);
+	child_process2(fd, pipefd, argv, env);
 	close(pipefd[1]);
 	close(pipefd[0]);
-	close(fd_in);
-	close(fd_out);
-	waitpid(pid0, &status, 0);
-	waitpid(pid1, &status, 0);
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid0, NULL, 0);
+	waitpid(pid1, NULL, 0);
 }
